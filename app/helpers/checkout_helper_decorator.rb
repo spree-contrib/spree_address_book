dc2b82db78bc679b9e2645347ce471dd74d08968
Eduardo Dominguez <eduardodmz@gmail.com>
2011-07-04 19:08:59 -0500

    - changing class to ApplicationHelper, otherwise crashes when editing an address while not in the checkout process

--- app/helpers/checkout_helper_decorator.rb
+++ app/helpers/checkout_helper_decorator.rb
@@ -1,4 +1,4 @@
-CheckoutHelper.module_eval do
+ApplicationHelper.module_eval do
   def address_field(form, method, id_prefix = "b", &handler)
     content_tag :p, :id => [id_prefix, method].join, :class => "field" do
       if handler
