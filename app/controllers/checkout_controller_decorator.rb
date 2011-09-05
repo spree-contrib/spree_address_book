b456599c0c5c6f3cd37ec0b920039b1e4010bab0
Roman Smirnov <roman@railsdog.com>
2011-09-05 22:16:38 +0400

    Improved conditions with help of additional guard condition

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -24,14 +24,15 @@ CheckoutController.class_eval do
     return unless params[:state] == "address" && @order.bill_address_id && @order.ship_address_id
     @order.bill_address.reload
     @order.ship_address.reload
+    # ensure that there is no validation errors and addresses was saved
+    return unless @order.bill_address && @order.ship_address
+    
     if @order.bill_address_id != @order.ship_address_id && @order.bill_address.same_as?(@order.ship_address)
       @order.bill_address.destroy
       @order.update_attribute(:bill_address_id, @order.ship_address.id)
-    elsif @order.bill_address
+    else
       @order.bill_address.update_attribute(:user_id, current_user.try(:id))
     end
-    if @order.ship_address
-      @order.ship_address.update_attribute(:user_id, current_user.try(:id))
-    end
+    @order.ship_address.update_attribute(:user_id, current_user.try(:id))
   end
 end
