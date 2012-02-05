8644b50c6e00bd74cc520bf2e04959fee467f019
George F Murphy <gfmurphy@gmail.com>
2012-02-04 23:55:42 -0600

    change table name in migrations.  add index on user_id

--- db/migrate/20110302102208_add_user_id_and_deleted_at_to_addresses.rb
+++ db/migrate/20110302102208_add_user_id_and_deleted_at_to_addresses.rb
@@ -1,15 +1,19 @@
 class AddUserIdAndDeletedAtToAddresses < ActiveRecord::Migration
   def self.up
-    change_table :addresses do |t|
+    change_table :spree_addresses do |t|
       t.integer :user_id
       t.datetime :deleted_at
     end
+
+    add_index :spree_addresses, :user_id
   end
 
   def self.down
-    change_table :addresses do |t|
+    remove_index :spree_addresses, :user_id
+
+    change_table :spree_addresses do |t|
       t.remove :deleted_at
-      t.remove :user_id    
+      t.remove :user_id
     end
   end
 end
