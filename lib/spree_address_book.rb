c45ff1c9cde0ee6c398078d8f4dd9fac1f96731b
George F Murphy <gfmurphy@gmail.com>
2012-02-04 23:46:07 -0600

    move engine into engine file

--- lib/spree_address_book.rb
+++ lib/spree_address_book.rb
@@ -1,19 +1,4 @@
 require 'spree_core'
 require 'spree_auth'
+require 'spree_address_book/engine'
 
-module SpreeAddressBook
-  class Engine < Rails::Engine
-    engine_name 'spree_address_book'
-
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
-      end
-      Spree::Ability.register_ability(AddressAbility)
-    end
-
-    config.to_prepare &method(:activate).to_proc
-  end
-end
