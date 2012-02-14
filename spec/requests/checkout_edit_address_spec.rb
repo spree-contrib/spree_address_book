b4ab3a86a0f08ef24795898896a725362e9cf007
George F Murphy <gfmurphy@gmail.com>
2012-02-13 21:04:36 -0600

    place commit button inside form

--- spec/requests/checkout_edit_address_spec.rb
+++ spec/requests/checkout_edit_address_spec.rb
@@ -5,7 +5,8 @@ describe "User editing saved address during checkout", :js => true do
   include_context "checkout with product"
   let(:state) { Spree::State.find_by_name('Arkansas') }
   let(:address) do
-    Factory(:address, :address1 => Faker::Address.street_address,
+    Factory(:address,
+      :address1 => Faker::Address.street_address,
       :state => state)
   end
   let(:user) do
@@ -17,17 +18,11 @@ describe "User editing saved address during checkout", :js => true do
 
   before(:each) { click_link "Checkout"; sign_in!(user) }
 
-  it "should click edit to edit billing address" do
+  it "can update billing address" do
     within("#billing #spree_address_#{address.id}") do
       click_link "Edit"
     end
-    page.should have_selector("form#edit_spree_address_#{address.id}")
-  end
-
-  it "should update billing address during checkout" do
-    within("#billing #spree_address_#{address.id}") do
-      click_link "Edit"
-    end
+    current_path.should == spree.edit_address_path(address)
     new_street = Faker::Address.street_address
     fill_in "Street Address", :with => new_street
     click_button "Update"
@@ -38,18 +33,11 @@ describe "User editing saved address during checkout", :js => true do
     end
   end
 
-  it "should click edit to edit shipping address" do
+  it "can update shipping address" do
     within("#shipping #spree_address_#{address.id}") do
       click_link "Edit"
     end
     current_path.should == spree.edit_address_path(address)
-    page.should have_selector("form#edit_spree_address_#{address.id}")
-  end
-
-  it "should update shipping address during checkout" do
-    within("#shipping #spree_address_#{address.id}") do
-      click_link "Edit"
-    end
     new_street = Faker::Address.street_address
     fill_in "Street Address", :with => new_street
     click_button "Update"
