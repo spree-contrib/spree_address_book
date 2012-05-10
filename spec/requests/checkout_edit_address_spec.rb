779220e2118671608bd884d5f39077f07b6820b0
Michael Bianco <iloveitaly@gmail.com>
2012-05-10 12:55:33 -0400

    checkout_edit_address_spec working fine. Deleted unneeded partials from other branch

--- spec/requests/checkout_edit_address_spec.rb
+++ spec/requests/checkout_edit_address_spec.rb
@@ -1,39 +1,39 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe "User editing saved address during checkout", :js => true do
-#   include_context "store products"
-#   include_context "checkout with product"
-#   include_context "user with address"
+describe "User editing saved address during checkout", :js => true do
+  include_context "store products"
+  include_context "checkout with product"
+  include_context "user with address"
 
-#   before(:each) { click_link "Checkout"; sign_in!(user) }
+  before(:each) { click_link "Checkout"; sign_in!(user) }
 
-#   it "can update billing address" do
-#     within("#billing #spree_address_#{address.id}") do
-#       click_link "Edit"
-#     end
-#     current_path.should == spree.edit_address_path(address)
-#     new_street = Faker::Address.street_address
-#     fill_in "Street Address", :with => new_street
-#     click_button "Update"
-#     current_path.should == spree.checkout_path
-#     within("h3") { page.should have_content("Checkout") }
-#     within("#billing") do
-#       page.should have_content(new_street)
-#     end
-#   end
+  it "can update billing address" do
+    within("#billing #billing_address_#{address.id}") do
+      click_link "Edit"
+    end
+    current_path.should == spree.edit_address_path(address)
+    new_street = Faker::Address.street_address
+    fill_in "Street Address", :with => new_street
+    click_button "Update"
+    current_path.should == spree.checkout_path
+    within("h1") { page.should have_content("Checkout") }
+    within("#billing") do
+      page.should have_content(new_street)
+    end
+  end
 
-#   it "can update shipping address" do
-#     within("#shipping #spree_address_#{address.id}") do
-#       click_link "Edit"
-#     end
-#     current_path.should == spree.edit_address_path(address)
-#     new_street = Faker::Address.street_address
-#     fill_in "Street Address", :with => new_street
-#     click_button "Update"
-#     current_path.should == spree.checkout_path
-#     within("h3") { page. should have_content("Checkout") }
-#     within("#shipping") do
-#       page.should have_content(new_street)
-#     end
-#   end
-# end
+  it "can update shipping address", :js => true do
+    within("#shipping #shipping_address_#{address.id}") do
+      click_link "Edit"
+    end
+    current_path.should == spree.edit_address_path(address)
+    new_street = Faker::Address.street_address
+    fill_in "Street Address", :with => new_street
+    click_button "Update"
+    current_path.should == spree.checkout_path
+    within("h1") { page.should have_content("Checkout") }
+    within("#shipping") do
+      page.should have_content(new_street)
+    end
+  end
+end
