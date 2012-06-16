164644875d77abe6272f1f69dadbfcc4794c6379
Roman Smirnov <roman@railsdog.com>
2012-06-16 19:40:05 +0400

    Double require should be avoided Fixes #6

--- lib/spree_address_book.rb
+++ lib/spree_address_book.rb
@@ -12,7 +12,7 @@ module Spree
       config.autoload_paths += %W(#{config.root}/lib)
 
       def self.activate
-        Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Dir.glob(File.join(File.dirname(__FILE__), "../app/**/spree/*_decorator*.rb")) do |c|
           Rails.env.production? ? require(c) : load(c)
         end
         Spree::Ability.register_ability(Spree::AddressAbility)
