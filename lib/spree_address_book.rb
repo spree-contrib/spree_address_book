d1da04e94e536267c7fcacb49ba8540a110afac9
Roman Smirnov <roman@railsdog.com>
2012-07-03 13:37:07 +0400

    Restored manual decorators loading

--- lib/spree_address_book.rb
+++ lib/spree_address_book.rb
@@ -12,6 +12,9 @@ module Spree
       config.autoload_paths += %W(#{config.root}/lib)
 
       def self.activate
+        Dir.glob(File.join(File.dirname(__FILE__), "../app/**/spree/*_decorator*.rb")) do |c|
+          Rails.application.config.cache_classes ? require(c) : load(c)
+        end
         Spree::Ability.register_ability(Spree::AddressAbility)
       end
 
