fc16a0dd9e6b933919b6172ef28301a02adeabfe
Michael Bianco <iloveitaly@gmail.com>
2012-10-10 15:58:14 -0400

    Added company field. Disabled alternative field in tests to pass more tests.

--- config/initializers/address_fields.rb
+++ config/initializers/address_fields.rb
@@ -1,1 +1,1 @@
-ADDRESS_FIELDS = ["firstname", "lastname", "address1", "address2", "city", "state", "zipcode", "country", "phone"]
+ADDRESS_FIELDS = ["firstname", "lastname", "company", "address1", "address2", "city", "state", "zipcode", "country", "phone"]
