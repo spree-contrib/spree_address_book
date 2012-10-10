557a9c4d89560adff08fae6862e1c2e81aaccf26
Michael Bianco <iloveitaly@gmail.com>
2012-10-10 15:41:13 -0400

    Updating engine initializer to new spree extension format. Removing Gemfile.lock.

---
+++ lib/spree_address_book/engine.rb
@@ -0,0 +1,25 @@
+module Spree
+  module AddressBook
+    class Engine < Rails::Engine
+      require 'spree/core'
+      isolate_namespace Spree
+      engine_name 'spree_address_book'
+      
+      initializer "spree.advanced_cart.environment", :before => :load_config_initializers do |app|
+        Spree::AddressBook::Config = Spree::AddressBookConfiguration.new
+      end
+      
+      config.autoload_paths += %W(#{config.root}/lib)
+
+      def self.activate
+        Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/spree/*_decorator*.rb")) do |c|
+          Rails.application.config.cache_classes ? require(c) : load(c)
+        end
+
+        Spree::Ability.register_ability(Spree::AddressAbility)
+      end
+
+      config.to_prepare &method(:activate).to_proc
+    end
+  end
+end
