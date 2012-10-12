d1529c268e7f0e5f32ff2fbf06be64e9de7414cd
Michael Bianco <iloveitaly@gmail.com>
2012-10-12 16:37:36 -0400

    Elminated unneeded config options. Updated HTML to match spree core. More specs passing.

--- app/models/spree/address_book_configuration.rb
+++
@@ -1,8 +0,0 @@
-module Spree
-  class AddressBookConfiguration < Preferences::Configuration
-    preference :disable_bill_address, :boolean, :default => false
-    preference :alternative_bill_address_phone, :boolean, :default => false
-    preference :alternative_ship_address_phone, :boolean, :default => false
-    preference :alternative_address_phone, :boolean, :default => false
-  end
-end
