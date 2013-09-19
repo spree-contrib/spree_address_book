b7afd98d6674edeabaf4c3dfaae98e6db089033a
abhishek <abhishek.jain@vinsol.com>
2013-09-19 18:39:42 +0530

    change address action

--- app/controllers/spree/addresses_controller.rb
+++ app/controllers/spree/addresses_controller.rb
@@ -4,7 +4,21 @@ class Spree::AddressesController < Spree::StoreController
   load_and_authorize_resource :class => Spree::Address
 
   def index
-    redirect_to account_path
+    @addresses = spree_current_user.addresses.not_deleted.includes(:state, :country)
+  end
+
+  def set_default_address
+    @success = @address.set_as_default!
+  end
+
+  def create
+    @address = spree_current_user.addresses.build(params[:address])
+    if @address.save
+      flash[:notice] = I18n.t(:successfully_created, :resource => I18n.t(:address))
+      redirect_to account_path
+    else
+      render :action => "new"
+    end
   end
   
   def show
@@ -37,17 +51,6 @@ class Spree::AddressesController < Spree::StoreController
       else
         render :action => "edit"
       end
-    end
-  end
-
-  def create
-    @address = Spree::Address.new(params[:address])
-    @address.user = current_user
-    if @address.save
-      flash[:notice] = I18n.t(:successfully_created, :resource => I18n.t(:address))
-      redirect_to account_path
-    else
-      render :action => "new"
     end
   end
 
