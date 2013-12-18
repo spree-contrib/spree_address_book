884697ffbc42f96698859d9e33a9499b44923190
Jonathan Tapia <jonathan.tapia@crowdint.com>
2013-12-18 17:42:48 -0600

    Add possibility to run migration automatically

--- lib/generators/spree_address_book/install_generator.rb
+++ lib/generators/spree_address_book/install_generator.rb
@@ -1,21 +1,23 @@
 module SpreeAddressBook
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, :type => :boolean, :default => false
+
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require store/spree_address_book\n"
       end
-      
+
       def add_stylesheets
         inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_address_book\n", :before => /\*\//, :verbose => true
       end
-    
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_address_book'
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
+         res = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask "Would you like to run the migrations now? [Y/n]")
+         if res
            run 'bundle exec rake db:migrate'
          else
            puts "Skiping rake db:migrate, don't forget to run it!"
