a8d94f12cd1dc8fa7eafd69801dd7cc6d60ba20a
Roman Smirnov <POMAHC@gmail.com>
2014-01-29 22:00:16 +0400

    Update has_many args to Rails 4 syntax

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,3 +1,3 @@
 Spree.user_class.class_eval do
-  has_many :addresses, :conditions => {:deleted_at => nil}, :order => "updated_at DESC", :class_name => 'Spree::Address'
+  has_many :addresses, -> { where(:deleted_at => nil).order("updated_at DESC") }, :class_name => 'Spree::Address'
 end
