2fb99fd6d75fb05e5dcd4defeea881475dfeb7ba
Roman Smirnov <POMAHC@gmail.com>
2014-02-05 12:38:04 +0400

    Additional fixes to work with Spree 2.1

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -1,19 +1,18 @@
 module Spree::AddressesHelper
-  def address_field(form, method, id_prefix = "b", &handler)
-    id_prefix = id_prefix == 'bill_address' ? 'b' : 's'
-    content_tag :p, :id => [id_prefix, method].join, :class => "field" do
+  def address_field(form, method, address_id = "b", &handler)
+    content_tag :p, :id => [address_id, method].join, :class => "field" do
       if handler
         handler.call
       else
         is_required = Spree::Address.required_fields.include?(method)
-        separator = is_required ? '<span class="req">*</span><br />' : '<br />'
+        separator = is_required ? '<span class="required">*</span><br />' : '<br />'
         form.label(method) + separator.html_safe +
         form.text_field(method, :class => is_required ? 'required' : nil)
       end
     end
   end
 
-  def address_state(form, country)
+  def address_state(form, country, address_id)
     country ||= Spree::Country.find(Spree::Config[:default_country_id])
     have_states = !country.states.empty?
     state_elements = [
@@ -27,7 +26,8 @@ module Spree::AddressesHelper
                       :disabled => have_states)
       ].join.gsub('"', "'").gsub("\n", "")
 
-    form.label(:state, Spree.t(:state)) + '<span class="req">*</span><br />'.html_safe +
+    form.label(:state, Spree.t(:state)) + 
+      %Q(<span class="required" id="#{address_id}state-required">*</span><br />).html_safe +
       content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
       javascript_tag("document.write(\"#{state_elements.html_safe}\");")
   end
