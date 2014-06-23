d2d7f8c8db8557aa63d08f18c9f7906d56d27bf6
Thomas I. Sample <thomas@mfrwdesign.com>
2014-06-23 11:18:19 -0400

    Updated and moved js and scss assets to their correct location in spree 2-2-stable

--- app/assets/javascripts/spree/frontend/spree_address_book.js
+++ app/assets/javascripts/spree/frontend/spree_address_book.js
@@ -1,15 +1,14 @@
-
 (function($) {
   $(document).ready(function(){
     if ($(".select_address").length) {
-      $('input#order_use_billing').unbind("click");
+      $('input#order_use_billing').unbind("change");
       $(".inner").hide();
       $(".inner input").prop("disabled", true);
       $(".inner select").prop("disabled", true);
       if ($('input#order_use_billing').is(':checked')) {
         $("#shipping .select_address").hide();
       }
-      
+
       $('input#order_use_billing').click(function() {
         if ($(this).is(':checked')) {
           $("#shipping .select_address").hide();
@@ -39,13 +38,13 @@
       });
     }
   });
-  
+
   function hide_address_form(address_type){
     $("#" + address_type + " .inner").hide();
     $("#" + address_type + " .inner input").prop("disabled", true);
     $("#" + address_type + " .inner select").prop("disabled", true);
   }
-  
+
   function show_address_form(address_type){
     $("#" + address_type + " .inner").show();
     $("#" + address_type + " .inner input").prop("disabled", false);
