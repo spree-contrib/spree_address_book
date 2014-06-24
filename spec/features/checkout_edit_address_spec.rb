d56bf9e25b54d894c121da06ea8df300a571041d
Thomas I. Sample <thomas@mfrwdesign.com>
2014-06-23 21:07:09 -0400

    Updated address_book translations in features checkout edit address spec

--- spec/features/checkout_edit_address_spec.rb
+++ spec/features/checkout_edit_address_spec.rb
@@ -14,7 +14,7 @@ describe "User editing saved address during checkout", :js => true do
     end
     current_path.should == spree.edit_address_path(address)
     new_street = Faker::Address.street_address
-    fill_in I18n.t('activerecord.attributes.spree/address.address1'), :with => new_street
+    fill_in Spree.t(:address1), :with => new_street
     click_button "Update"
     current_path.should == spree.checkout_state_path('address')
     within("h1") { page.should have_content("Checkout") }
@@ -29,7 +29,7 @@ describe "User editing saved address during checkout", :js => true do
     end
     current_path.should == spree.edit_address_path(address)
     new_street = Faker::Address.street_address
-    fill_in I18n.t('activerecord.attributes.spree/address.address1'), :with => new_street
+    fill_in Spree.t(:address1), :with => new_street
     click_button "Update"
     current_path.should == spree.checkout_state_path('address')
     within("h1") { page.should have_content("Checkout") }
