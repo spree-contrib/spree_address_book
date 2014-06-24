e58531cc2973d1bedd12ca6e0d4281be4b01832e
Thomas I. Sample <thomas@mfrwdesign.com>
2014-06-23 20:40:39 -0400

    Fixed undefined method on_hand error

--- spec/support/store_products.rb
+++ spec/support/store_products.rb
@@ -8,11 +8,9 @@ shared_context "store products" do
     mugs_taxon = FactoryGirl.create(:taxon, :name => 'Mugs', :parent_id => root.id)
 
     FactoryGirl.create(:custom_product, :name => 'Ruby on Rails Ringer T-Shirt',
-      :price => '17.99', :taxons => [clothing_taxon],
-      :on_hand => 1)
+      :price => '17.99', :taxons => [clothing_taxon])
     FactoryGirl.create(:custom_product, :name => 'Ruby on Rails Mug', :price => '13.99',
-      :taxons => [mugs_taxon],
-      :on_hand => 10)
+      :taxons => [mugs_taxon])
   end
 end
 
