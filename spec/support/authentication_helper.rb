4a264910e0e66dc3b66251d2f1a1c09db1d87ee6
Thomas I. Sample <thomas@mfrwdesign.com>
2014-06-23 21:16:30 -0400

    Added sign_in_to_cart! to spec support authentication helper

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -4,6 +4,15 @@ module Authentication
     fill_in "Password", :with => "secret"
     click_button "Login"
   end
+
+  def sign_in_to_cart!(user)
+    visit "/login"
+    fill_in "Email", :with => user.email
+    fill_in "Password", :with => "secret"
+    click_button "Login"
+    visit "/cart"
+    click_button "Checkout"
+  end
 end
 
 RSpec.configure do |c|
