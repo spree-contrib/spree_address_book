45c813ef5c44a54ef11441c3790a695ea3c91d3c
Michael Bianco <iloveitaly@gmail.com>
2014-07-13 08:22:38 -0400

    Cleaning up address model spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,54 +1,45 @@
 require 'spec_helper'
 
 describe Spree::Address do
+  let(:address) { FactoryGirl.create(:address) }
+  let(:address2) { FactoryGirl.create(:address) }
+  let(:order) { FactoryGirl.create(:completed_order_with_totals) }
+  let(:user) { FactoryGirl.create(:user) }
 
-  # Test spree_address_book's address_decorator.rb.
-  describe 'spree_address_book address_decorator' do
-    let(:address) { FactoryGirl.create(:address) }
-    let(:address2) { FactoryGirl.create(:address) }
-    let(:order) { FactoryGirl.create(:order) }
-    let(:user) { FactoryGirl.create(:user) }
+  before { order.update_attribute(:bill_address, address2) }
 
-    before {
-      order.bill_address = address2
-      order.save
-    }
-
-    it 'has required attributes' do
-      Spree::Address.required_fields.should eq([:firstname, :lastname, :address1, :city, :zipcode, :country, :phone])
-    end
-
-    it 'is editable' do
-      address.should be_editable
-    end
-
-    it 'can be deleted' do
-      address.should be_can_be_deleted
-    end
-
-    it "isn't editable when there is an associated order" do
-      # TODO need to simulate completing an order
-      # address2.should_not be_editable
-    end
-
-    it "can't be deleted when there is an associated order" do
-      address2.should_not be_can_be_deleted
-    end
-
-    it 'is displayed as string' do
-      a = address
-      address.to_s.should eq("#{a.firstname} #{a.lastname}<br/>#{a.address1}<br/>#{a.address2}<br/>#{a.city}, #{a.state || a.state_name} #{a.zipcode}<br/>#{a.country}".html_safe)
-    end
-
-    it 'is destroyed without saving used' do
-      address.destroy
-      Spree::Address.where(["id = (?)", address.id]).should be_empty
-    end
-
-    it 'is destroyed deleted timestamp' do
-      address2.destroy
-      Spree::Address.where(["id = (?)", address2.id]).should_not be_empty
-    end
+  it 'has required attributes' do
+    Spree::Address.required_fields.should eq([:firstname, :lastname, :address1, :city, :zipcode, :country, :phone])
   end
 
+  it 'is editable' do
+    address.should be_editable
+  end
+
+  it 'can be deleted' do
+    address.should be_can_be_deleted
+  end
+
+  it "isn't editable when there is an associated order" do
+    address2.should_not be_editable
+  end
+
+  it "can't be deleted when there is an associated order" do
+    address2.should_not be_can_be_deleted
+  end
+
+  it 'is displayed as string' do
+    a = address
+    address.to_s.should eq("#{a.firstname} #{a.lastname}<br/>#{a.address1}<br/>#{a.address2}<br/>#{a.city}, #{a.state || a.state_name} #{a.zipcode}<br/>#{a.country}".html_safe)
+  end
+
+  it 'is destroyed without saving used' do
+    address.destroy
+    Spree::Address.where(["id = (?)", address.id]).should be_empty
+  end
+
+  it 'is destroyed deleted timestamp' do
+    address2.destroy
+    Spree::Address.where(["id = (?)", address2.id]).should_not be_empty
+  end
 end
