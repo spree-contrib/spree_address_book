8938dc4822fe9eaf262b0fc0974d2200ba9918e9
Jason Stokes <jpstokes@plecco.net>
2014-08-06 21:51:17 -0500

    add logic to add user_id to address when saved

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,3 +1,5 @@
 Spree.user_class.class_eval do
-  has_many :addresses, -> { where(:deleted_at => nil).order("updated_at DESC") }, :class_name => 'Spree::Address'
+  has_many :addresses, -> { where(:deleted_at => nil).order("updated_at DESC") },
+    :class_name => 'Spree::Address', inverse_of: :user
+  accepts_nested_attributes_for :addresses
 end
