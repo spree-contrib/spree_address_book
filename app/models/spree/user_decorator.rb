92772c3247cdd1b88fd2c585d4aef30ffe6ef240
Jason Stokes <jpstokes@plecco.net>
2014-08-06 21:54:06 -0500

    return file to original version

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,3 @@
 Spree.user_class.class_eval do
-  has_many :addresses, -> { where(:deleted_at => nil).order("updated_at DESC") },
-    :class_name => 'Spree::Address', inverse_of: :user
-  accepts_nested_attributes_for :addresses
+  has_many :addresses, -> { where(:deleted_at => nil).order("updated_at DESC") }, :class_name => 'Spree::Address'
 end
