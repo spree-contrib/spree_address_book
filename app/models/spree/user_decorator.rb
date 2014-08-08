97ff7170d1cd1374531a82f80585cf49dc3d22ea
Jason Stokes <jpstokes@plecco.net>
2014-08-08 14:24:36 -0500

    corrected function call

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,7 +4,7 @@ Spree.user_class.class_eval do
   has_many :addresses, -> { where(:deleted_at => nil).order("updated_at DESC") }, :class_name => 'Spree::Address'
 
   def link_address
-    bill_address.update(user_id: id) if bill_address
-    ship_address.update(user_id: id) if ship_address
+    bill_address.update_attributes(user_id: id) if bill_address
+    ship_address.update_attributes(user_id: id) if ship_address
   end
 end
