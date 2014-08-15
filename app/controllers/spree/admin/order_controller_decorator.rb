2181ebac203b4835a5a4e732be7480fc6cab509d
Jason Stokes <jpstokes@plecco.net>
2014-08-15 17:55:02 -0500

    add new view for address and controller

--- app/controllers/spree/admin/order_controller_decorator.rb
+++ app/controllers/spree/admin/order_controller_decorator.rb
@@ -1,9 +1,10 @@
 Spree::Admin::OrdersController.class_eval do
   def addresses
-    order = Spree::Order.find_by_number(params[:id])
+    @order = Spree::Order.find_by_number(params[:id])
     country_id = Spree::Address.default.country.id
-    @user = order.user
+    @user = @order.user
     @user.build_bill_address(:country_id => country_id)
     @user.build_ship_address(:country_id => country_id)
+    render 'spree/admin/addresses/addresses'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Spree::Core::Engine.add_routes do
   namespace :admin do
     resources :orders do
-      member do
-        get :addresses
-      end
+      resources :addresses, only: [:index, :create]
     end
   end
 
