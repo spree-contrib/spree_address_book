5b615493d768c35382f9a56c5bf315b52538dfd1
Jason Stokes <jpstokes@plecco.net>
2014-08-15 14:32:30 -0500

    add code to render address

--- app/controllers/spree/admin/order_controller_decorator.rb
+++ app/controllers/spree/admin/order_controller_decorator.rb
@@ -1,8 +1,9 @@
 Spree::Admin::OrdersController.class_eval do
   def addresses
     order = Spree::Order.find_by_number(params[:id])
+    country_id = Spree::Address.default.country.id
     @user = order.user
-    @user.build_ship_address
-    @user.build_bill_address
+    @user.build_bill_address(:country_id => country_id)
+    @user.build_ship_address(:country_id => country_id)
   end
 end
