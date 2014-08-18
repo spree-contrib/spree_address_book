af60cfaa61877c930a6b17b8343e858ef80db6e8
Jason Stokes <jpstokes@plecco.net>
2014-08-18 17:15:28 -0500

    removed unused code

--- app/controllers/spree/admin/order_controller_decorator.rb
+++ app/controllers/spree/admin/order_controller_decorator.rb
@@ -9,15 +9,6 @@ Spree::Admin::OrdersController.class_eval do
     redirect_to edit_admin_order_url(@order)
   end
 
-  def addresses
-    @order = Spree::Order.find_by_number(params[:id])
-    country_id = Spree::Address.default.country.id
-    @user = @order.user
-    @user.build_bill_address(:country_id => country_id)
-    @user.build_ship_address(:country_id => country_id)
-    render 'spree/admin/addresses/addresses'
-  end
-
   private
 
   def order_params
