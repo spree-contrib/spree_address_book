4a6e2f3cb5f72946688244f308dcb5ae77fe58d8
Jason Stokes <jpstokes@plecco.net>
2014-09-02 20:10:13 -0500

    fix addresses on order page so they work with modified controller

--- app/controllers/spree/admin/addresses_controller.rb
+++ app/controllers/spree/admin/addresses_controller.rb
@@ -2,8 +2,13 @@ module Spree
   module Admin
     class AddressesController < ResourceController
       def index
-        @user = Spree::User.find(params[:user_id])
         @order = Spree::Order.find_by(number: params[:order_id])
+        @user =
+          if @order
+            @order.user
+          else
+            Spree::User.find(params[:user_id])
+          end
 
         @default_user_addresses_hash = {}
 
@@ -15,59 +20,47 @@ module Spree
           if @user.ship_address
             @default_user_addresses_hash[I18n.t(:shipping_address, scope: :address_book)] = @user.ship_address
           end
+        end
 
-          if @order
-            @current_order_addresses_hash = {}
-            if @order.bill_address
-              @current_order_addresses_hash[I18n.t(:billing_address, scope: :address_book)] = @order.bill_address
+        if @order
+          @current_order_addresses_hash = {}
+          if @order.bill_address
+            @current_order_addresses_hash[I18n.t(:billing_address, scope: :address_book)] = @order.bill_address
+          end
+
+          if @order.ship_address
+            @current_order_addresses_hash[I18n.t(:shipping_address, scope: :address_book)] = @order.ship_address
+          end
+
+          previous_order = Spree::Order.where(user_id: @user.id).order(:created_at).last
+
+          if previous_order != @order
+            @previous_order_addresses_hash= {}
+            if previous_order.bill_address
+              @previous_order_addresses_hash[I18n.t(:billing_address, scope: :address_book)] = previous_order.bill_address
             end
 
-            if @order.ship_address
-              @current_order_addresses_hash[I18n.t(:shipping_address, scope: :address_book)] = @order.ship_address
-            end
-
-            previous_order = Spree::Order.where(user_id: @user.id).order(:created_at).last
-
-            if previous_order != @order
-              @previous_order_addresses_hash= {}
-              if previous_order.bill_address
-                @previous_order_addresses_hash[I18n.t(:billing_address, scope: :address_book)] = previous_order.bill_address
-              end
-
-              if previous_order.ship_address
-                @previous_order_addresses_hash[I18n.t(:shipping_address, scope: :address_book)] = previous_order.ship_address
-              end
+            if previous_order.ship_address
+              @previous_order_addresses_hash[I18n.t(:shipping_address, scope: :address_book)] = previous_order.ship_address
             end
           end
         end
 
-        if @order.present?
-        else
-          render 'spree/admin/users/user_addresses'
-        end
+        render 'spree/admin/users/user_addresses' unless @order
       end
 
       def new
-        @order = Spree::Order.find_by_number(params[:order_id])
+        @user = Spree::User.find(params[:user_id])
+        @order = Spree::Order.find_by_number(params[:order])
         country_id = Spree::Address.default.country.id
-        @user =
-          if @order
-            @order.user
-          else
-            Spree::User.find(params[:user_id])
-          end
         @address = @user.addresses.new(:country_id => country_id)
       end
 
       def create
-        @order = Spree::Order.find_by_number(params[:order_id])
-        @user =
-          if @order
-            @order.user
-          else
-            Spree::User.find(params[:user_id])
-          end
+        @order = Spree::Order.find_by_number(params[:order])
+        @user = Spree::User.find(params[:user_id])
         @address = @user.addresses.new(address_params)
+
         if @address.save
           @user.save_default_addresses(
             params[:address_default_bill],
@@ -86,11 +79,19 @@ module Spree
           flash.now[:success] = Spree.t(:account_updated)
         end
 
-        redirect_to admin_user_addresses_url(@user)
+        path =
+          if @order
+            admin_order_addresses_url @order
+          else
+            admin_user_addresses_url @user
+          end
+
+        redirect_to path
       end
 
       def edit
         @address = Spree::Address.find(params[:id])
+        @order = Spree::Order.find_by(number: params[:order])
         @user = @address.user
       end
 
@@ -105,7 +106,7 @@ module Spree
             @address
           )
 
-          @order = Spree::Order.find_by_number(params[:order_id])
+          @order = Spree::Order.find_by_number(params[:order])
 
           if @order
             @order.save_current_order_addresses(
@@ -118,7 +119,13 @@ module Spree
           flash[:success] = "Address updated successfully"
         end
 
-        redirect_to admin_user_addresses_url @user
+        path =
+          if @order
+            admin_order_addresses_url @order
+          else
+            admin_user_addresses_url @user
+          end
+        redirect_to path
       end
 
       private
