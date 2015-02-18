c03632cb81970fb0c444c60eca08cd6e720c0dcd
Roman Smirnov <POMAHC@gmail.com>
2015-02-18 15:05:09 +0300

    Revert "Removing unneeded helper that can be included via core"
    This reverts commit 3fa7fe127bcbb08ce7722107df55234c731af57b.

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -11,5 +11,24 @@ module Spree::AddressesHelper
       end
     end
   end
+
+  def address_state(form, country)
+    country ||= Spree::Country.find(Spree::Config[:default_country_id])
+    have_states = !country.states.empty?
+    state_elements = [
+      form.collection_select(:state_id, country.states.order(:name),
+                            :id, :name,
+                            {:include_blank => true},
+                            {:class => have_states ? "required" : "hidden",
+                            :disabled => !have_states}) +
+      form.text_field(:state_name,
+                      :class => !have_states ? "required" : "hidden",
+                      :disabled => have_states)
+      ].join.gsub('"', "'").gsub("\n", "")
+
+    form.label(:state, t(:state)) + '<span class="req">*</span><br />'.html_safe +
+      content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
+      javascript_tag("document.write(\"#{state_elements.html_safe}\");")
+  end
 end
 
