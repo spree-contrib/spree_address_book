308f5e6739e051de2ae0b8424c3a19a119e8fbff
Roman Smirnov <POMAHC@gmail.com>
2015-02-19 22:51:00 +0300

    Fixed issue with using 'Other address'

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -74,18 +74,19 @@ Spree::Order.class_eval do
   def update_or_create_address(attributes = {})
     return if attributes.blank?
     attributes = attributes.select{|k,v| v.present?}.permit(permitted_address_attributes)
-    
+
     if self.user
-      address = self.user.addresses.build(attributes).check
+      address = self.user.addresses.build(attributes.except(:id)).check
       return address if address.id
     end
 
     if attributes[:id]
       address = Spree::Address.find(attributes[:id])
-      #attributes.delete(:id)
+      attributes.delete(:id)
 
       if address && address.editable?
         address.update_attributes(attributes)
+        return address
       else
         attributes.delete(:id)
       end
