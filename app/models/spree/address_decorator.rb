db0860e57f893b067bff759c8871541237385066
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-09 16:59:24 +0800

    Escape address fields in `#to_s` method

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -10,14 +10,14 @@ Spree::Address.class_eval do
       v.kind_of?(ActiveModel::Validations::PresenceValidator) ? v.attributes : []
     end.flatten
   end
-  
+
   # TODO: look into if this is actually needed. I don't want to override methods unless it is really needed
   # can modify an address if it's not been used in an order
   def same_as?(other)
     return false if other.nil?
     attributes.except('id', 'updated_at', 'created_at', 'user_id') == other.attributes.except('id', 'updated_at', 'created_at', 'user_id')
   end
-  
+
   # can modify an address if it's not been used in an completed order
   def editable?
     new_record? || (shipments.empty? && Spree::Order.complete.where("bill_address_id = ? OR ship_address_id = ?", self.id, self.id).count == 0)
@@ -29,13 +29,13 @@ Spree::Address.class_eval do
 
   def to_s
     [
-      "#{firstname} #{lastname}",
-      company.to_s,
-      address1.to_s,
-      address2.to_s,
-      "#{city}, #{state ? state.abbr : state_name} #{zipcode}",
+      full_name,
+      company,
+      address1,
+      address2,
+      "#{city}, #{state_text} #{zipcode}",
       country.to_s
-    ].reject(&:empty?).join("<br/>").html_safe
+    ].reject(&:empty?).map{ |attribute| ERB::Util.html_escape(attribute) }.join('<br/>')
   end
 
   # UPGRADE_CHECK if future versions of spree have a custom destroy function, this will break
