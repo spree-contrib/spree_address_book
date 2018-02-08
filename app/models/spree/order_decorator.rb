de45865584869c9a28e12995e0ca953a758fec37
wuboy <wuboys@gmail.com>
2018-02-09 03:23:57 +0800

    fix clone_billing_address for new spree

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,15 +6,19 @@ Spree::Order.class_eval do
   before_validation :clone_shipping_address, :if => "Spree::AddressBook::Config[:disable_bill_address]"
   
   def clone_shipping_address
-    if self.ship_address
-      self.bill_address = self.ship_address
+    if ship_address && bill_address.nil?
+      self.bill_address = ship_address.clone
+    else
+      bill_address.attributes = ship_address.attributes.except('id', 'updated_at', 'created_at')
     end
     true
   end
   
   def clone_billing_address
-    if self.bill_address
-      self.ship_address = self.bill_address
+    if bill_address && ship_address.nil?
+      self.ship_address = bill_address.clone
+    else
+      ship_address.attributes = bill_address.attributes.except('id', 'updated_at', 'created_at')
     end
     true
   end
