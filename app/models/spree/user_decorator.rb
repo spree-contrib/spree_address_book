e57952e2a57d84d6df00538e521f2639197c9c8f
hf2186 <hf2186@users.noreply.github.com>
2018-09-12 14:41:22 -0400

    Adding a Fix For Lookup by user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,3 +1,5 @@
 Spree.user_class.class_eval do
   has_many :addresses, -> { where(deleted_at: nil).order('updated_at DESC') }, class_name: Spree::Address
+  belongs_to :ship_address, -> { where(deleted_at: nil) }, class_name: 'Spree::Address'
+  belongs_to :bill_address, -> { where(deleted_at: nil) }, class_name: 'Spree::Address'
 end
